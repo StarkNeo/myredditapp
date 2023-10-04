174a00b360df5c702015d54d5d3dd40b526d698f
Jesus Hernandez <jesushdzusa@gmail.com>
2023-10-04 12:55:35 -0500

    object with comments identified

--- src/services/apiconnect.js
+++ src/services/apiconnect.js
@@ -9,6 +9,9 @@ listing = controversial, best, hot, new, random, rising, top
 
 //JSON URL: https://www.reddit.com/r/popular.json
 JSON URL: https://www.reddit.com/search.json?q=cake%20recipes
+
+https://www.reddit.com/{subreddit_name_prefixed}/comments/{id}/what_is_something_that_smells_great_but_isnt/
+
 */
 export const getComments = async () => {
     const response = await fetch('https://www.reddit.com/r/popular.json')
@@ -16,13 +19,17 @@ export const getComments = async () => {
     return json.data.children
 }
 
-
+/*
 export const search = async () => {
-    const response = await fetch(`https://www.reddit.com/search.json?q=cake`)
+    const response = await fetch(`https://www.reddit.com/r/Jujutsushi/comments/jujutsu_kaisen_chapter_238_prerelease_leaks_thread`);
     const json = await response.json();
-    console.log(json);
+    return json
+}*/
+
+const getPosts = async ()=>{
+    const response = await fetch("https://www.reddit.com/r/WhitePeopleTwitter/comments/16zk082/these_little_piggies_would_appreciate_it_if.json");
+    const json = response.json();
+    return json;
 }
-
 export let arreglo = await getComments();
-
-console.log(arreglo);
+export let posts = await getPosts();
